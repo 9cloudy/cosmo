8fc8c0f68b4168a0c2338792581b57c018cc7564
Aarush-sharma <aarush.sharma.fpw@gmail.com>
2025-01-19 22:38:47 +0530

    added: card menu to show pending requests

--- apps/web/app/find/page.tsx
+++ apps/web/app/find/page.tsx
@@ -1,12 +1,11 @@
 
 import QueryForm from "@repo/ui/query";
 
-// put the name and image and user id in seprate so the {} thse wotn show up in the button
 export default function AddFriends() {
   
   return (
     <>
-  <QueryForm></QueryForm>
+    <QueryForm></QueryForm>
     </>
   );
 }
